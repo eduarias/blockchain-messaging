aa0a42d6d66d83d192b6d7a68e59c883bab021b8
earias <eduarias@users.noreply.github.com>
2018-09-12 18:54:50 +0200

    mining endpoints

--- node.py
+++ node.py
@@ -34,3 +34,21 @@ def get_chain():
         chain_data.append(block.__dict__)
     return json.dumps({'length': len(chain_data),
                        'chain': chain_data})
+
+
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+    if not result:
+        return 'No transactions to mine'
+    else:
+        return 'Block #{} is mined.'.format(result)
+
+
+@app.route('/pending_tx')
+def get_pending_tx():
+    """Get unconfirmed transactions"""
+    return json.dumps(blockchain.unconfirmed_transactions)
+
+
+app.run(debug=True, port=8000)
